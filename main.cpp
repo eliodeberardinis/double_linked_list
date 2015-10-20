8bdaf7ca713f1c01a5e3b46e8e8b0154bea82053
lios87 <eliodeb@gmail.com>
2015-10-20 19:08:35 +0100

    completing the exercise
    Thanks to JP and Mircea

--- main.cpp
+++ main.cpp
@@ -4,10 +4,10 @@
 // This is your 
 struct item {
   // link to next item in the list or "head"
-  item *prev;
+  item *next;
 
   // link to the previous item or "head"
-  item *next;
+  item *prev;
 
   // the value in the list
   int value;
@@ -19,36 +19,56 @@ struct item {
 class double_linked_list {
 public:
   // placeholder for you to replace.
-  nullptr_t WHAT = nullptr;
+  item *Tail = nullptr;
 
   // The head item is the first and last in the list.
   // head <-> one <-> two <-> three <-> head
   item head;
 
   double_linked_list() {
-    // at the start, the head points to itself.
+    // at the start, the head points to itself.CONSTRUCTOR
     head.prev = head.next = &head;
   }
 
   item *insert(int value) {
     item *new_item = new item(value);
     new_item->prev = &head;
-    new_item->next = WHAT; // fill this in
-    head.prev = WHAT; // fill this in
-    head.next = new_item;
+    new_item->next = head.next; // fill this in
+	head.next->prev = new_item;
+    
+	if (head.prev == &head)
+	{
+		head.prev =new_item;
+	} 
+	// fill this in
+		head.next = new_item;
+	
+		return new_item;
   }
 
   item *insert_after(item *prev, int value) {
     item *next = prev->next;
     item *new_item = new item(value);
-    new_item->prev = &head;
-    new_item->next = WHAT; // fill this in
-    prev->next = WHAT; // fill this in
-    next->prev = WHAT; // fill this in
+    new_item->prev = prev;
+    new_item->next = next; // fill this in
+    prev->next = new_item; // fill this in
+    next->prev = new_item; // fill this in
+
+	return new_item;
   }
 
   item *find(int value) {
      // write a loop here to return the first element with this value
+	  item *index = head.next;
+	  while (index != &head)
+	  {
+		  if ((index->value) == value)
+
+			  return index;
+		  index = index->next;
+	  }
+
+	  return nullptr;
   }
 
   item *get_first() {
@@ -57,11 +77,11 @@ public:
 
   void remove(item *victim) {
       item *prev = victim->prev;
-      item *next = victim->prev;
-      victim->prev = WHAT; // fill this in
-      victim->next = WHAT; // fill this in
-      prev->next = WHAT; // fill this in
-      next->prev = WHAT; // fill this in
+      item *next = victim->next;
+      victim->prev = nullptr; // fill this in
+      victim->next = nullptr; // fill this in
+      prev->next = next; // fill this in
+      next->prev = prev; // fill this in
   }
 
   void dump(std::ostream &os) {
